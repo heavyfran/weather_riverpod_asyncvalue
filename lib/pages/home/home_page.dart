f5d9e00d1c7eddba4c900537d6e758b0798bce14
heavyfran <heavyfran@gmail.com>
2023-11-14 20:28:34 +0900

    SearchPage, fetchWeather in HomePage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../extensions/async_value_xx.dart';
+import '../search/search_page.dart';
 import 'providers/weather_provider.dart';
 
 class HomePage extends ConsumerStatefulWidget {
@@ -12,6 +13,8 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  String? city;
+
   @override
   Widget build(BuildContext context) {
     final weatherState = ref.watch(weatherProvider);
@@ -20,6 +23,22 @@ class _HomePageState extends ConsumerState<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              city = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+              print('city: $city');
+              if (city != null) {
+                ref.read(weatherProvider.notifier).fetchWeather(city!);
+              }
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
