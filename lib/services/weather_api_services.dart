634d22f80b3aec7f58ffd20e424168064dd2b2fc
heavyfran <heavyfran@gmail.com>
2023-11-14 17:29:48 +0900

    WeatherApiServices 2, weatherApiServicesProvider

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../constants/constants.dart';
 import '../exceptions/weather_exception.dart';
+import '../models/current_weather/current_weather.dart';
 import '../models/direct_geocoding/direct_geocoding.dart';
 import 'dio_error_handler.dart';
 
@@ -33,6 +34,31 @@ class WeatherApiServices {
       final directGeocoding = DirectGeocoding.fromJson(response.data[0]);
 
       return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<CurrentWeather> getWeather(DirectGeocoding directGeocoding) async {
+    try {
+      final Response response = await dio.get(
+        '/data/2.5/weather',
+        queryParameters: {
+          'lat': '${directGeocoding.lat}',
+          'lon': '${directGeocoding.lon}',
+          'units': kUnit,
+          'appid': dotenv.env['APPID'],
+        },
+      );
+
+      if (response.statusCode != 200) {
+        throw dioErrorHandler(response);
+      }
+
+      final CurrentWeather currentWeather =
+          CurrentWeather.fromJson(response.data);
+
+      return currentWeather;
     } catch (e) {
       rethrow;
     }
